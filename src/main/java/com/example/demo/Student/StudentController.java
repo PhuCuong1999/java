5a270ce3fce52797d82b7a50def52d98aa9c0bdd
unknown <cv9cuong@gmail.com>
2021-04-22 23:20:03 +0700

    Lan 2

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -1,11 +1,22 @@
 package com.example.demo.Student;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.DataInput;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 //@RequestMapping(path="api/v1/student")
@@ -22,9 +33,25 @@ public class StudentController {
         return studentService.getStudent();
     }
 
-    @PostMapping
-    public void registerNewStudent(@RequestBody Student student){
-        studentService.addNewStudent(student);
+    @PostMapping({"/Post"})
+    public void registerNewStudent(@RequestBody String st) throws JsonProcessingException, JSONException {
+
+        JSONObject jsonObj = new JSONObject(st);
+        String name = jsonObj.getString("name");
+        
+        LocalDate dt = LocalDate.parse(jsonObj.getString("dob"));
+
+        String mail = jsonObj.getString("email");
+        Student a = new Student(name, dt, mail);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        Student s = objectMapper.readValue(st, Student.class);
+
+//        int age = jsonObj.getInt("age");
+//        System.out.println(age);
+          studentService.addNewStudent(a);
+
+//        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+//        String json = ow.writeValueAsString(a);
     }
 
     @GetMapping({"/Home"})
